c5b064e192b18370c91fe048cd34e7b3acc001dd
Islam Ahmed <ra.work99@gmail.com>
2021-07-05 10:35:22 +0200

    v1

--- session2/custom.js
+++ session2/custom.js
@@ -1,4 +1,5 @@
 let showHide = document.querySelector('#showHide')
+let data = []
 let formHeads = [
     'taskTitle',
      "taskType",
@@ -10,12 +11,23 @@ showHide.addEventListener('click', function(e){
     this.textContent == "Show" ? this.textContent='Hide': this.textContent='Show'
 })
 
+const writeDataInLocalStorage = ( data ) =>{
+    localStorage.setItem('notes', JSON.stringify(data))
+}
+saveData = (note)=>{
+    
+    data.push(note)
+    writeDataInLocalStorage(data)
+    }
 document.querySelector('#form-div form').addEventListener('submit', function(e){
     e.preventDefault()
     task = { status:false }
     formHeads.forEach(h => {
             task[h]= this.elements[h].value
     })
+    saveData(task)
     console.log(task)
 })
+
+
 test()
